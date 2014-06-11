ff35c8868e5a140b846974249e90f1ef4db55408
Swapnil Singh <swapnil@embibe.com>
2014-06-11 20:31:35 +0530

    fixes injection bug

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,3 +1,4 @@
 humpYoutube.controller('MainController',['$scope','$location',function($scope, $location){
   $scope.videoId = $location.search().v || '8UVNT4wvIGY';
+  console.log($scope.videoId);
 }]);
--- js/script.js
+++ js/script.js
@@ -15,19 +15,21 @@ humpYoutube.config(['$routeProvider', '$locationProvider', function($routeProvid
 }]);
 humpYoutube.controller('MainController',['$scope','$location',function($scope, $location){
   $scope.videoId = $location.search().v || '8UVNT4wvIGY';
+  console.log($scope.videoId);
 }]);
 humpYoutube.directive('youtube', ['$sce', function($sce) {
   return {
     restrict: 'EA',
     scope: { id: '=' },
     template: '<iframe width="560" height="315" src="{{url}}" frameborder="0" allowfullscreen></iframe>',
-    link: ['$scope', function (scope) {
+    link: function (scope) {
 
         scope.$watch('id', function (newVal) {
            if (newVal) {
+              console.log(newVal)
                scope.url = $sce.trustAsResourceUrl("https://www.youtube.com/v/"+ newVal + "?version=3&loop=1&autoplay=1&playlist="+ newVal);
            }
         });
-    }]
+    }
   };
 }]);
